ae9b2d01ae84501d75f5240ad596ffb47bf08552
Alexandre <alexandre@users.noreply.github.com>
2018-01-24 16:26:42 +0100

    Ajout des classes Hachage et TestHachage

---
+++ baseserver/src/main/java/org/orbisgis/orbisserver/baseserver/model/TestHachage.java
@@ -0,0 +1,21 @@
+package org.orbisgis.orbisserver.baseserver.model;
+
+public class TestHachage {
+
+    public static void main(String [] args){
+        testSha256();
+    }
+
+    public static void testSha256(){
+
+        System.out.println("[----------------- Test de la méthode sha256 -----------------]\n");
+        System.out.println("Hachage de la chaîne de caractère : password");
+        System.out.println("Hash : "+Hachage.sha256("password")+"\n");
+        String passwordSha256 = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+        if(passwordSha256.equals(Hachage.sha256("password"))){
+            System.out.println("TEST OK");
+        } else{
+            System.out.println("TEST PAS OK");
+        }
+    }
+}
